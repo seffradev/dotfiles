0c33eea1973ce36731cef5ff44477dd93dc1948c
Hampus Avekvist <hampus.avekvist@hey.com>
2024-07-22 02:55:32 +0200

    feat(nvim): remove timeout stuffs from which-key

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,10 +1,6 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
     keys = {
         { "<leader>g",  group = "Git",                                                                     desc = "Git",                                          nowait = true, remap = false },
         { "<leader>gs", "<cmd>Git<cr>",                                                                    desc = "Show git status",                              nowait = true, remap = false },
