f1fff41df25343ad943388816d7fe7dba1346d07
Hampus Avekvist <hampus.avekvist@hey.com>
2024-08-10 20:27:05 +0200

    feat(nvim): add twilight.nvim and make zen-mode more zen

--- nvim/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/plugins/zen-mode.lua
@@ -1,8 +1,21 @@
 return {
     "folke/zen-mode.nvim",
+    dependencies = {
+        "folke/twilight.nvim",
+    },
     opts = {
         window = {
-            width = 80,
+            width = 90,
+            backdrop = 1,
+            options = {
+                signcolumn = "no",  -- disable signcolumn
+                number = true,     -- disable number column
+                relativenumber = false, -- disable relative numbers
+                cursorline = false, -- disable cursorline
+                cursorcolumn = false, -- disable cursor column
+                foldcolumn = "0",   -- disable fold column
+                list = false,       -- disable whitespace characters
+            },
         },
     },
 }
