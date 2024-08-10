b24c92a478c267de42c417dedd27d4ca8aba445d
Hampus Avekvist <hampus.avekvist@hey.com>
2024-08-11 00:04:16 +0200

    refactor(nvim): move keybindings to appropriate configuration files
    - Move files keybindings to files.lua
    - Move telescope keybindings to search.lua
    - Move lsp keybindings to lsp.lua
    - Move git keybindings to git.lua
    - Separate accessibility bindings for easier config file reading

--- nvim/.config/nvim/plugin/git.lua
+++ nvim/.config/nvim/plugin/git.lua
@@ -1,3 +1,18 @@
 require("git-worktree").setup({
     fetch_on_create = false,
 })
+
+-- Bindings
+local wk = require("which-key")
+
+wk.add({
+    { "<leader>g",  group = "Git",  desc = "Git",             nowait = true, remap = false },
+    { "<leader>gs", "<cmd>Git<cr>", desc = "Show git status", nowait = true, remap = false },
+})
+
+--- git-worktree
+wk.add({
+    { "<leader>w",  group = "Worktree",                                                                desc = "",       nowait = true, remap = false },
+    { "<leader>wc", "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", desc = "Create", nowait = true, remap = false },
+    { "<leader>ws", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>",       desc = "Switch", nowait = true, remap = false },
+})
