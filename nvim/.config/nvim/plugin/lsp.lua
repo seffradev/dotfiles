21b71e4da1cfe5c19d4938529fad65f3e7d13b6a
Hampus Avekvist <hampus.avekvist@hey.com>
2024-12-01 01:53:26 +0100

    style(nvim): Format

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -139,14 +139,16 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 -- Trouble
-vim.keymap.set("n", "<leader>tx", "<cmd>Trouble diagnostic toggle<cr>", { desc = "Diagnostics (Trouble)" })
-vim.keymap.set("n", "<leader>tX", "<cmd>Trouble diagnostics toggle filter.buf=1<cr>", { desc = "Buffer Diagnostics (Trouble)" })
 wk.add({
     { "<leader>t", group = "Trouble", desc = "", nowait = true, remap = false },
 })
 
+vim.keymap.set("n", "<leader>tx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
+vim.keymap.set("n", "<leader>tX", "<cmd>Trouble diagnostics toggle filter.buf=1<cr>",
+    { desc = "Buffer Diagnostics (Trouble)" })
 vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
-vim.keymap.set("n", "<leader>tl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "LSP Definitions / references / ... (Trouble)" })
+vim.keymap.set("n", "<leader>tl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    { desc = "LSP Definitions / references / ... (Trouble)" })
 vim.keymap.set("n", "<leader>tL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
 vim.keymap.set("n", "<leader>tQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
 
