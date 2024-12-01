cd578e01ace11abcb7631ba39ab5d8bc9a835f1e
Hampus Avekvist <hampus.avekvist@hey.com>
2024-12-01 01:49:58 +0100

    refactor(nvim): Use `vim.keymap.set` instead of `which-key` to set bindings

--- nvim/.config/nvim/plugin/files.lua
+++ nvim/.config/nvim/plugin/files.lua
@@ -1,20 +1,13 @@
 -- Bindings
-local wk = require("which-key")
-
-wk.add({
-    { "<leader>p",  group = "Project", desc = "",                  nowait = true, remap = false },
-    { "<leader>pv", vim.cmd.Ex,        desc = "Show project view", nowait = true, remap = false },
-})
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Show project view" })
 
 --- Harpoon
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-wk.add({
-    { "<leader>a", mark.add_file,                 desc = "Add current file to Harpoon list" },
-    { "<leader>h", ui.toggle_quick_menu,          desc = "Toggle Harpoon quick menu" },
-    { "<A-h>",     function() ui.nav_file(1) end, desc = "Go to 1st marked file" },
-    { "<A-j>",     function() ui.nav_file(2) end, desc = "Go to 2nd marked file" },
-    { "<A-k>",     function() ui.nav_file(3) end, desc = "Go to 3rd marked file" },
-    { "<A-l>",     function() ui.nav_file(4) end, desc = "Go to 4th marked file" },
-})
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add current file to Harpoon list" })
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = "Toggle Harpoon quick menu" })
+vim.keymap.set("n", "<A-h>", function() ui.nav_file(1) end, { desc = "Go to 1st marked file" })
+vim.keymap.set("n", "<A-j>", function() ui.nav_file(2) end, { desc = "Go to 2nd marked file" })
+vim.keymap.set("n", "<A-k>", function() ui.nav_file(3) end, { desc = "Go to 3rd marked file" })
+vim.keymap.set("n", "<A-l>", function() ui.nav_file(4) end, { desc = "Go to 4th marked file" })
