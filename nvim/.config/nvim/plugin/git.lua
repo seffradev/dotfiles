58fbb2a050fde4d5d4863d37785dc90be7c9b433
Hampus Avekvist <hampus.avekvist@hey.com>
2024-12-01 01:55:31 +0100

    feat(nvim)!: Move git worktree bindings under `Git` group

--- nvim/.config/nvim/plugin/git.lua
+++ nvim/.config/nvim/plugin/git.lua
@@ -12,5 +12,5 @@ require("git-worktree").setup({
 vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Show git status" })
 
 -- git-worktree
-vim.keymap.set("n", "<leader>wc", "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", { desc = "Create Git Worktree" })
-vim.keymap.set("n", "<leader>ws", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", { desc = "Switch Git Worktree" })
+vim.keymap.set("n", "<leader>gwc", "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<cr>", { desc = "Create Git Worktree" })
+vim.keymap.set("n", "<leader>gws", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<cr>", { desc = "Switch Git Worktree" })
