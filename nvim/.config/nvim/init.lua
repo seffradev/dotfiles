837cb3384e10ec9fb748f7c80d8e9e46aac9b66f
Hampus Avekvist <hampus.avekvist@hey.com>
2025-04-26 19:28:29 +0200

    feat(nvim): Remove broken keybinds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,10 +43,6 @@ keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up" })
 keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down" })
 keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
     { desc = "Substitute word under cursor" })
-keymap.set("n", "<leader>S", "V:s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
-    { desc = "Substitute word under cursor on current line" })
-keymap.set("v", "<leader>s", "\"my:%s/\\<<C-r>m\\>/<C-r>m/gI<Left><Left><Left>",
-    { desc = "Substitute selection" })
 
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
