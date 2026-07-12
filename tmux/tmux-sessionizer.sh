86513b85ef62cef10bb37d01dd0e22980d62cb42
Hampus Avekvist <hampus@seffra.dev>
2026-07-12 13:02:21 +0200

    feat(tmux): Remove `~/.config` from `tmux-sessionizer.sh`

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/.config ~/projects ~/ /home -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/projects ~/ /home -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
